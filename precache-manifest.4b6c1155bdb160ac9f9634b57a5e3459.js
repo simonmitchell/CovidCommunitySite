7abed869ef0bd9ae72877107de7f2a3ed57289e9
Simon Mitchell <simon@3sidedcube.com>
2020-03-20 21:07:31 +0000

    Updates

---
+++ precache-manifest.4b6c1155bdb160ac9f9634b57a5e3459.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22f689959e4e8d36b8072971626631c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7caf7b0fef46a6079796",
+    "url": "/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "feebb08c577d357097db",
+    "url": "/static/css/main.ef345176.chunk.css"
+  },
+  {
+    "revision": "7caf7b0fef46a6079796",
+    "url": "/static/js/2.203f35e9.chunk.js"
+  },
+  {
+    "revision": "feebb08c577d357097db",
+    "url": "/static/js/main.36fac477.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
